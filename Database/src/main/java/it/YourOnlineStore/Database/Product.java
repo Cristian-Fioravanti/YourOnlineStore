1ada58897d879f3a23cfbc2e258e1c6e75226b15
Kanton99 <avolkov99@gmail.com>
2022-05-05 15:22:36 +0200

    product database, add, all and buy interfaces added

---
+++ Database/src/main/java/it/YourOnlineStore/Database/Product.java
@@ -0,0 +1,43 @@
+package it.YourOnlineStore.Database;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer productId;
+
+    private String productName;
+    private Float cost;
+    private Integer disponibiliy;
+    private String description;
+    private String imageUrl;
+
+    public Integer getProductId() {return this.productId;}
+
+    public String getProductName(){return this.productName;}
+    public void setProductName(String productName){this.productName = productName;}
+
+    public Float getCost(){return this.cost;}
+    public void setCost(Float cost){this.cost = cost;}
+
+    public Integer getDisponibiliy(){return this.disponibiliy;}
+    public void setDisponibiliy(Integer disponibility) {this.disponibiliy = disponibility;}
+
+    public String getImageUrl(){return this.imageUrl;}
+    public void setImage(String imageUrl){
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription(){return this.description;}
+    public void setDescription(String description){this.description = description;}
+    public void buy(){this.disponibiliy--;}
+}
---
+++ Database/src/main/java/it/YourOnlineStore/Database/ProductRepository.java
@@ -0,0 +1,6 @@
+package it.YourOnlineStore.Database;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+}
