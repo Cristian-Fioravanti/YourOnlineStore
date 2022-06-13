c8d5475c596dd30c05b711bb12b1def12b8cd16e
Cristian Fioravanti <cristian.fioravanti@micegroup.it>
2022-06-13 11:34:54 +0200

    Inserimento primo front end

---
+++ modules/yourstore-be/yourstore-gateway/src/main/java/it/yourstore/gateway/controller/MainController.java
@@ -0,0 +1,25 @@
+package it.yourstore.gateway.controller;
+
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/prova")
+    public String prova() {
+		return "prova";
+    } 
+}
---
+++ modules/yourstore-be/yourstore-gateway/src/main/resources/static/js/custom.js
@@ -0,0 +1,18 @@
+test = document.getElementById("dropdownMenu2")
+header = document.getElementById("header")
+test.addEventListener("mouseover", function( event ) {
+    // highlight the mouseenter target
+    setTimeout(function() {
+        event.target.click()
+      }, 600);
+    
+    
+  }, false);
+
+
+function resetWhenLeave(event,obj){
+    obj.addEventListener("mouseleave",function(event2){
+        event.target.click()
+    },false)
+}
+
