2cc663bfc59a2c5c15aad76f0ca011b97b69b4f7
Cristian Fioravanti <cristian.fioravanti@micegroup.it>
2022-09-29 18:59:02 +0200

    aggiunte pagine base

--- modules/yourstore-be/yourstore-gateway/src/main/java/it/yourstore/gateway/controller/MainController.java
+++ modules/yourstore-be/yourstore-gateway/src/main/java/it/yourstore/gateway/controller/MainController.java
@@ -11,8 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
@@ -22,4 +25,40 @@ public class MainController {
     public String prova() {
 		return "prova";
     } 
+	
+	@RequestMapping("/home")
+    public String getHome() {
+		return "home";
+    } 
+	
+	@RequestMapping("/cart")
+    public String getCarello() {
+		return "cart";
+    } 
+	
+	@RequestMapping("/product")
+    public String getProdotto() {
+		return "product";
+    } 
+	
+	@RequestMapping("/shipping")
+    public String getShipping() {
+		return "shipping";
+    } 
+	
+	@RequestMapping("/listprod")
+    public String getListProduct() {
+		return "listProduct";
+    } 
+	
+//	@PostMapping("/addPoduct")
+//	   public String saveProduct(@RequestParam("file") MultipartFile file,
+//	   		@RequestParam("pname") String name,
+//	   		@RequestParam("price") int price,
+//	   		@RequestParam("desc") String desc)
+//	   {
+//	   	productService.saveProductToDB(file, name, desc, price);
+//	   	return "redirect:/home";
+//	   }
+	
 }
