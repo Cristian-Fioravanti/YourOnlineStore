881857ea2077b44f39c427aeb61bf8e22e6fa026
Cristian Fioravanti <cristian.fioravanti@micegroup.it>
2022-10-04 17:14:10 +0200

    fix activeMQ

--- yourstore-ms-store/src/main/java/it/yourstore/store/YourstoreMsStore.java
+++ yourstore-ms-store/src/main/java/it/yourstore/store/YourstoreMsStore.java
@@ -3,11 +3,15 @@ package it.yourstore.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import it.yourstore.store.jmsClient.AzioniJMSListener;
 
 @SpringBootApplication
 @ComponentScan({ "it.yourstore.store"})
 public class YourstoreMsStore {
 	public static void main(String[] args) {
 		SpringApplication.run(YourstoreMsStore.class, args);
+		AzioniJMSListener aJMSL = new AzioniJMSListener();
+		aJMSL.start();
+
 	}
 }
